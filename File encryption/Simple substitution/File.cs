b46e73d737772e24d9e88b66c2d44c9ea950a595
Vladimir <132492186+Rukati@users.noreply.github.com>
2023-09-27 14:26:51 +0300

    Add files via upload

---
+++ File encryption/Simple substitution/File.cs
@@ -0,0 +1,93 @@
+﻿using _2lb_3k_1s;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _2lb_3k_1s
+{
+    internal class File
+    {
+        private string folderPath { get; set; }
+        public File(string folderPath)
+        {
+            this.folderPath = folderPath;
+        }
+        public void ReadFiles(ref List<(byte[], string)> filesData)
+        {
+            try
+            {
+                if (Directory.Exists(folderPath))
+                {
+                    var files = Directory.GetFiles(folderPath);
+
+                    foreach (var filePath in files)
+                    {
+                        byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+                        string fileName = Path.GetFileName(filePath);
+                        filesData.Add((fileBytes, fileName));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Указанной папки не существует.");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Произошла ошибка при доступе к папке: " + e.Message);
+            }
+        }
+        public byte[] ReadKeyFromFile(string keyFilePath)
+        {
+            try
+            {
+                if (System.IO.File.Exists(keyFilePath))
+                {
+                    // Считываем содержимое файла с ключом
+                    byte[] keyBytes = System.IO.File.ReadAllBytes(keyFilePath);
+                    Console.WriteLine("Ключ успешно считан из файла.");
+                    return keyBytes;
+                }
+                else
+                {
+                    Console.WriteLine("Указанный файл ключа не существует.");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Произошла ошибка при чтении файла ключа: " + e.Message);
+            }
+
+            return null;
+        }
+        public void SaveKeyToFile(byte[] key, string keyFilePath)
+        {
+            try
+            {
+                // Записываем ключ в файл
+                System.IO.File.WriteAllBytes(keyFilePath + "\\key.txt", key);
+                Console.WriteLine("Ключ успешно сохранен в файле.");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Произошла ошибка при сохранении ключа в файле: " + e.Message);
+            }
+        }
+        public void SaveFile(byte[] encryptedByte, string filePathSave, string fileName)
+        {
+            try
+            {
+                string encryptedFilePath = Path.Combine(filePathSave, fileName);
+                Console.WriteLine($"Файл {fileName} успешно зашифрован и сохранен в {encryptedFilePath}");
+                System.IO.File.WriteAllBytes(encryptedFilePath, encryptedByte);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Произошла ошибка при сохранении файла: " + e.Message);
+            }
+        }
+
+    }
+}
---
+++ File encryption/Simple substitution/SimpleSubstitutionCipher.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace _2lb_3k_1s
+{
+    internal class SimpleSubstitutionCipher
+    {
+        static private byte[] encryptionKey; // Ключ для шифрования
+        static public byte[] decryptionKey; // Ключ для дешифрования
+
+        public SimpleSubstitutionCipher(byte[] key = null)
+        {
+            if (key == null)
+                GenerateKeys();
+            else
+                decryptionKey = key;
+
+        }
+        private static void GenerateKeys()
+        {
+            try
+            {
+                // Создаем массив с числами от 0 до 255
+                byte[] values = Enumerable.Range(0, 256).Select(i => (byte)i).ToArray();
+
+                // Перемешиваем значения
+                Random rng = new Random();
+                int n = values.Length;
+                while (n > 1)
+                {
+                    n--;
+                    int k = rng.Next(n + 1);
+                    byte temp = values[k];
+                    values[k] = values[n];
+                    values[n] = temp;
+                }
+
+                // Создаем ключ для шифрования
+                encryptionKey = values.ToArray();
+
+                // Создаем ключ для дешифрования (обратная подстановка)
+                decryptionKey = new byte[256];
+                for (int i = 0; i < 256; i++)
+                {
+                    decryptionKey[encryptionKey[i]] = (byte)i;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка при генерации ключей: " + ex.Message);
+                throw;
+            }
+        }
+        public byte[] Encrypt(byte[] data)
+        {
+            if (data == null || data.Length == 0)
+            {
+                throw new ArgumentException("Входные данные не могут быть пустыми.");
+            }
+
+            byte[] encryptedBytes = new byte[data.Length];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                encryptedBytes[i] = encryptionKey[data[i]];
+            }
+
+            return encryptedBytes;
+        }
+        public byte[] Decrypt(byte[] encryptedData)
+        {
+            if (encryptedData == null || encryptedData.Length == 0)
+            {
+                throw new ArgumentException("Зашифрованные данные не могут быть пустыми.");
+            }
+
+            byte[] decryptedBytes = new byte[encryptedData.Length];
+
+            for (int i = 0; i < encryptedData.Length; i++)
+            {
+                decryptedBytes[i] = decryptionKey[encryptedData[i]];
+            }
+
+            return decryptedBytes;
+        }
+        public static void PrintBytes(byte[] bytes)
+        {
+            Console.WriteLine("Байты:");
+            Console.WriteLine(BitConverter.ToString(bytes));
+
+            Console.WriteLine(); // Добавьте дополнительный перенос строки в конце
+        }
+        private static bool AreBytesEqual(byte[] byteArray1, byte[] byteArray2)
+        {
+            if (byteArray1.Length != byteArray2.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+
+                if (byteArray1[i] != byteArray2[i])
+                    return false;
+
+            }
+
+            return true;
+        }
+        public static void Examination(byte[] fileBytes, string fileName, byte[] decryptedBytes)
+        {
+            // Проверяем, совпадают ли исходные байты и дешифрованные байты
+            bool decryptionSuccessful = AreBytesEqual(fileBytes, decryptedBytes);
+
+            if (decryptionSuccessful)
+            {
+                Console.WriteLine($"Файл {fileName} успешно зашифрован и дешифрован.");
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка дешифрования файла {fileName}.");
+            }
+        }
+    }
+}
